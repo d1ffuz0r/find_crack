8f0f31f9fad959cb3b0b2c4a22a778d6423aa980
d1ffuz0r <gladk0w@mail.ru>
2011-06-06 01:12:56 +0400

    create project

---
+++ find.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import urllib2
+import re
+
+class FindCrack(object):
+	'''
+	search cracks on http://mskd-ru.net
+	''''
+	def __init__(self):
+		self.result = []
+		
+	def search(self,name,count=1000):
+		'''
+		name = name program
+		count = number result, default - 1000
+		''''
+		page = urllib2.urlopen('http://mskd-ru.net/apteka.php?crack=%s' % name ).read()
+		self.parse(page)
+		return self.result[:int(count)]
+		
+	def parse(self,html):
+		for j in re.findall(r'(<a target=_blank href="\./redir.php\?(.*?)">(.*?)</a>\s::\s(.*?)\s::\s(.*?)<br>)',html):
+			res = list(j)
+			self.result.append(dict(url=res[1],name=res[2], size=res[3], date=res[4]))
+		
+find = FindCrack()
+find.search('',10)
