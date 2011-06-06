fed9d7c8d65ed0f9d1c7af41338e035e1b0d416a
d1ffuz0r <gladk0w@mail.ru>
2011-06-05 22:33:37 -0700

    Edited find.py via GitHub

--- find.py
+++ find.py
@@ -15,14 +15,14 @@ class FindCrack(object):
 		name = name program
 		count = number result, default - 1000
 		''''
-		page = urllib2.urlopen('http://mskd-ru.net/apteka.php?crack=%s' % name ).read()
+		page = urllib2.urlopen('http://mskd-ru.net/apteka.php?crack=%s' % name).read()
 		self.parse(page)
 		return self.result[:int(count)]
 		
 	def parse(self,html):
 		for j in re.findall(r'(<a target=_blank href="\./redir.php\?(.*?)">(.*?)</a>\s::\s(.*?)\s::\s(.*?)<br>)',html):
 			res = list(j)
-			self.result.append(dict(url=res[1],name=res[2], size=res[3], date=res[4]))
+			self.result.append(dict(url=res[1],name=res[2],size=res[3],date=res[4]))
 '''		
 example use this class
 find = FindCrack()
