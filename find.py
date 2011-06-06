d7f2f2953ecf046cbc24271b0bf8c6d9db409248
d1ffuz0r <gladk0w@mail.ru>
2011-06-06 05:28:34 -0700

    Support multi-search. added www.crackserialkeygen .com in base search

--- find.py
+++ find.py
@@ -5,27 +5,39 @@ import re
 
 class FindCrack(object):
 	'''
-	search cracks on http://mskd-ru.net
-	''''
+	search cracks on http://mskd-ru.net,www.crackserialkeygen.com
+	'''
 	def __init__(self):
 		self.result = []
+		self.__all__ = [
+					{
+						'url':'http://mskd-ru.net/apteka.php?crack=%s',
+						'regexp':r'<a target=_blank\shref="\./redir.php\?(.*?)"\s(.*?)>(.*?)</a>\s::(.*?)\s::\s(.*?)<br>',
+						'rows':[0, 2]
+					},
+					{
+						'url':'http://www.crackserialkeygen.com/%s-crack-serial-keygen.html',
+						'regexp':r'<div class="result"><a class="slink" href="(.*?)">(.*?)</a>',
+						'rows':[0, 1]
+					}
+				]
 		
-	def search(self,name,count=1000):
+	def search(self, name, count=1000):
 		'''
-		name = name program
+		name = name what search
 		count = number result, default - 1000
-		''''
-		page = urllib2.urlopen('http://mskd-ru.net/apteka.php?crack=%s' % name).read()
-		self.parse(page)
+		'''
+		map(lambda param:self._parse(param, name), self.__all__)
 		return self.result[:int(count)]
 		
-	def parse(self,html):
-		for j in re.findall(r'(<a target=_blank href="\./redir.php\?(.*?)">(.*?)</a>\s::\s(.*?)\s::\s(.*?)<br>)',html):
-			res = list(j)
-			self.result.append(dict(url=res[1],name=res[2],size=res[3],date=res[4]))
-'''		
-example use this class
+	def _parse(self, data, name):
+		page = urllib2.urlopen(data['url'] % name).read()
+		for res in re.findall(data['regexp'], page, re.I):
+			self.result.append(dict(url=res[data['rows'][0]], name=res[data['rows'][1]]))
+
+'''
+example
 find = FindCrack()
-for cr in find.search('decompiler',10):
-	print cr
+for j in find.search('decompiler', 10):
+	print j
 '''
